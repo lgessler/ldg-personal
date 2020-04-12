4be3939d95b5f6816e5db8f9772316e64e3c703f
Luke Gessler <lukegessler@gmail.com>
2020-04-12 17:01:21 -0400

    include data

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author_email="lukegessler@gmail.com",
     url="https://github.com/lgessler/ldg-personal",
     packages=find_packages(),
+    include_package_data=True,
     version="0.0.1",
     install_requires=[
     ],
